331736ccc34e37ad0fac6f90d1f8c59a1252a376
nipsufn <ferdynand.naczynski@gmail.com>
2020-01-28 20:56:40 +0100

    initial commit

---
+++ epd7in5b.py
@@ -0,0 +1,279 @@
+# //*****************************************************************************
+# * | File        :	  epd7in5b.py
+# * | Author      :   Waveshare team
+# * | Function    :   Electronic paper driver
+# * | Info        :
+# *----------------
+# * |	This version:   V3.0
+# * | Date        :   2018-11-12
+# * | Info        :   python2 demo
+# * 1.Remove:
+#   digital_write(self, pin, value)
+#   digital_read(self, pin)
+#   delay_ms(self, delaytime)
+#   set_lut(self, lut)
+#   self.lut = self.lut_full_update
+# * 2.Change:
+#   display_frame -> TurnOnDisplay
+#   set_memory_area -> SetWindow
+#   set_memory_pointer -> SetCursor
+#   get_frame_buffer -> getbuffer
+#   set_frame_memory -> display
+# * 3.How to use
+#   epd = epd7in5b.EPD()
+#   epd.init(epd.lut_full_update)
+#   image = Image.new('1', (epd7in5b.EPD_WIDTH, epd7in5b.EPD_HEIGHT), 255)
+#   ...
+#   drawing ......
+#   ...
+#   epd.display(getbuffer(image))
+# ******************************************************************************//
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documnetation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and//or sell
+# copies of the Software, and to permit persons to  whom the Software is
+# furished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+
+import epdconfig
+from PIL import Image
+import RPi.GPIO as GPIO
+
+class Epd:
+    # Display resolution
+    Width                                   = 640
+    Height                                  = 384
+
+    # EPD7IN5 commands
+    PanelSetting                               = 0x00
+    PowerSetting                               = 0x01
+    PowerOff                                   = 0x02
+    PowerOffSequenceSetting                    = 0x03
+    PowerOn                                    = 0x04
+    PowerOnMeasure                             = 0x05
+    BoosterSoftStart                           = 0x06
+    DeepSleep                                  = 0x07
+    DataStartTransmission1                     = 0x10
+    DataStop                                   = 0x11
+    DisplayRefresh                             = 0x12
+    ImageProcess                               = 0x13
+    LutForVcom                                 = 0x20
+    LutBlue                                    = 0x21
+    LutWhite                                   = 0x22
+    LutGray1                                   = 0x23
+    LutGray2                                   = 0x24
+    LutRed0                                    = 0x25
+    LutRed1                                    = 0x26
+    LutRed2                                    = 0x27
+    LutRed3                                    = 0x28
+    LutXon                                     = 0x29
+    PllControl                                 = 0x30
+    TemperatureSensorCommand                   = 0x40
+    TemperatureCalibration                     = 0x41
+    TemperatureSensorWrite                     = 0x42
+    TemperatureSensorRead                      = 0x43
+    VcomAndDataIntervalSetting                 = 0x50
+    LowPowerDetection                          = 0x51
+    TconSetting                                = 0x60
+    TconResolution                             = 0x61
+    SpiFlashControl                            = 0x65
+    Revision                                   = 0x70
+    GetStatus                                  = 0x71
+    AutoMeasurementVcom                        = 0x80
+    ReadVcomValue                              = 0x81
+    VcmDcSetting                               = 0x82
+    
+    # LUT Tables
+    # VCOM (11 bytes)
+    LutC = bytearray([
+        
+        ])
+    # Black (13 bytes)
+    LutB = bytearray([
+        
+        ])
+    # White (13 bytes)
+    LutW = bytearray([
+        
+        ])
+    # Gray 1 (13 bytes)
+    LutG1 = bytearray([
+        0x01, 0x00
+        ])
+    # Gray 2 (13 bytes)
+    LutG2 = bytearray([
+        
+        ])
+    # Red 0 (13 bytes)
+    LutR0 = bytearray([
+        
+        ])
+    # Red 1 (13 bytes)
+    LutR1 = bytearray([
+        
+        ])
+    # Red 2 (13 bytes)
+    LutR2 = bytearray([
+        
+        ])
+    # Red 3 (13 bytes)
+    LutR3 = bytearray([
+        
+        ])
+    # XON (10 bytes)
+    LutX = bytearray([
+        
+        ])
+        
+    ColorLookupNamed = { "black": 0b000,
+                         "grey1": 0b001,
+                         "grey2": 0b010,
+                         "white": 0b011,
+                         "red0" : 0b100,
+                         "red1" : 0b101,
+                         "red2" : 0b110,
+                         "red3" : 0b111
+                         }
+    ColorLookupPalette8 = { 0: 0b000,
+                            1: 0b001,
+                            2: 0b010,
+                            3: 0b011,
+                            4: 0b100,
+                            5: 0b101,
+                            6: 0b110,
+                            7: 0b111
+                            }
+    ColorLookupPalette3 = { 0: 0b000,
+                            1: 0b011,
+                            2: 0b100
+                            }
+                         
+    def __init__(self):
+        self.reset_pin = epdconfig.RST_PIN
+        self.dc_pin = epdconfig.DC_PIN
+        self.busy_pin = epdconfig.BUSY_PIN
+
+    # Hardware reset
+    def reset(self):
+        # epdconfig.digital_write(self.reset_pin, GPIO.HIGH)
+        # epdconfig.delay_ms(200) 
+        epdconfig.digital_write(self.reset_pin, GPIO.LOW)         # module reset
+        epdconfig.delay_ms(200)
+        epdconfig.digital_write(self.reset_pin, GPIO.HIGH)
+        epdconfig.delay_ms(200)   
+
+    def send_command(self, command):
+        epdconfig.digital_write(self.dc_pin, GPIO.LOW)
+        epdconfig.spi_writebyte([command])
+
+    def send_data(self, data):
+        epdconfig.digital_write(self.dc_pin, GPIO.HIGH)
+        epdconfig.spi_writebyte([data])
+        
+    def wait_until_idle(self):
+        print("e-Paper busy")
+        while(epdconfig.digital_read(self.busy_pin) == 0):      # 0: busy, 1: idle
+            epdconfig.delay_ms(100)
+        print("e-Paper busy release")
+            
+    def init(self):
+        if (epdconfig.module_init() != 0):
+            return -1
+            
+        self.reset()
+        
+        self.send_command(self.PowerSetting)
+        self.send_data(0x37)
+        self.send_data(0x00)
+        self.send_command(self.PanelSetting)
+        self.send_data(0xCF)
+        self.send_data(0x08)
+        self.send_command(self.BoosterSoftStart)
+        self.send_data(0xc7)
+        self.send_data(0xcc)
+        self.send_data(0x28)
+        self.send_command(self.PowerOn)
+        self.wait_until_idle()
+        self.send_command(self.PllControl)
+        self.send_data(0x3c)
+        self.send_command(self.TemperatureCalibration)
+        self.send_data(0x00)
+        self.send_command(self.VcomAndDataIntervalSetting)
+        self.send_data(0x77)
+        self.send_command(self.TconSetting)
+        self.send_data(0x22)
+        self.send_command(self.TconResolution)
+        self.send_data(0x02)     #source 640
+        self.send_data(0x80)
+        self.send_data(0x01)     #gate 384
+        self.send_data(0x80)
+        self.send_command(self.VcmDcSetting)
+        self.send_data(0x1E)      #decide by LUT file
+        self.send_command(0xe5)           #FLASH MODE
+        self.send_data(0x03)
+
+        return 0
+
+    def pxmap(self, pixel):
+        pixel = pixel % 3
+        return self.pxmap3(pixel)
+
+    def pxmap3(self, pixel):
+        return self.ColorLookupPalette3[pixel]
+
+    def pxmap8(self, pixel):
+        return self.ColorLookupPalette8[pixel]
+
+    def SetLUTG2(self):
+        self.send_command(self.LutGray1)
+        for i in range(0, 19):
+            for byte in self.LutG1:
+                send_data(byte)
+        
+    def Display(self, image):
+        self.send_command(self.DataStartTransmission1)
+        imagedata = iter(list(image.getdata()))
+        for pixel in imagedata:
+            byte = self.pxmap(pixel)
+            byte = byte << 4
+            pixel = next(imagedata, 0)
+            byte = byte | self.pxmap(pixel)
+            self.send_data(byte)
+
+        self.send_command(self.DisplayRefresh)
+        epdconfig.delay_ms(100)
+        self.wait_until_idle()
+
+    def Clear(self, color="white"):
+        self.send_command(self.DataStartTransmission1)
+        byte = self.ColorLookupNamed[color]
+        byte = byte << 4
+        byte = byte | self.ColorLookupNamed[color]
+        for i in range(0, self.Width // 8 * self.Height):
+            self.send_data(byte)
+            self.send_data(byte)
+            self.send_data(byte)
+            self.send_data(byte)
+        self.send_command(self.DisplayRefresh)
+        epdconfig.delay_ms(100)
+        self.wait_until_idle()
+
+    def sleep(self):
+        self.send_command(self.PowerOff)
+        self.wait_until_idle()
+        self.send_command(self.DeepSleep)
+        self.send_data(0xA5)
+
---
+++ main.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.ticker as ticker
+from datetime import datetime, timedelta
+import time
+from signal import SIGINT, signal
+import traceback
+from io import BytesIO
+import epd7in5b
+import gc
+
+from PIL import Image,ImageDraw,ImageFont
+
+matplotlib.use('Agg')
+config = None
+with open("config.json", 'r') as configFile:
+    config = json.load(configFile)
+    
+kelvinOffset = config["kelvinOffset"]
+forecastLocation = config["forecastLocation"]
+forecastToken = config["forecastToken"]
+weatherLocations = config["weatherLocations"]
+weatherToken = config["weatherToken"]
+
+epd = epd7in5b.Epd()
+
+def elvis(a, b):
+    if a:
+        return a
+    return b
+
+def getJsonFromUrl(url, timeout=10):
+    try:
+        response = requests.get(url, timeout=timeout)
+        r.raise_for_status()
+    except requests.exceptions.ConnectionError as error:
+        print("HTTP connection error!\n"+str(error))
+        return None
+    except requests.exceptions.Timeout as error:
+        print("HTTP timeout!\n"+str(error))
+        return None
+    except requests.exceptions.HTTPError as error:
+        print("HTTP timeout!\n"+str(error))
+        return None
+    if response.content == None:
+        print("Undefined error!\n")
+        return None
+    return json.loads(response.content)
+    
+def stop():
+    epd.init();
+    epd.sleep();
+    exit();
+    
+signal(SIGINT, stop)
+if __name__ == "__main__":
+    counter = 0
+    epd.init()
+    epd.Clear("white")
+    PaletteImage = Image.open('palette_bwr_bodge.bmp')
+
+    #persist less frequent requests
+    forecastJson = None
+    weatherJson = None
+    jazzJson1 = None
+    jazzJson2 = None
+    forecastPlotImage = Image.new('RGB', (720, 200), (0xFF, 0xFF, 0xFF))
+    
+    while True:
+        epd.init()
+        jazzJson1 = elvis(getJsonFromUrl("https://croapi.cz/data/v2/schedule/now/1/jazz.json"), jazzJson1)
+        jazzJson2 = elvis(getJsonFromUrl("https://croapi.cz/data/v2/playlist/now/jazz.json"), jazzJson2)
+            
+        if counter % 10 == 0:
+            #those are updated infrequently, no point spamming APIs
+            forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" \
+                + forecastLocation \
+                + "&APPID=" \
+                + forecastToken
+            forecastJson = elvis(getJsonFromUrl(forecastUrl), forecastJson)
+            for weatherLocation in weatherLocations:
+                weatherUrl = "https://airapi.airly.eu/v2/measurements/installation?installationId=" \
+                    + weatherLocation \
+                    + "&apikey=" \
+                    + weatherToken
+                weatherJsonTmp = getJsonFromUrl(weatherUrl)
+                if weatherJsonTmp is not None and weatherJsonTmp['current']['indexes'][0]['value'] is not None:
+                    weatherJson = weatherJsonTmp
+                    break
+
+            #process forecast data
+            xAxisTimestamps = []
+            xAxisHours = []
+            yAxisTemperature =[]
+            yAxisPrecipitation = []
+            for timestamp in forecastJson['list']:
+                xAxisTimestamps.append(timestamp['dt'])
+                xAxisHours.append(datetime.fromtimestamp(timestamp['dt']))
+                yAxisTemperature.append(timestamp['main']['temp']-kelvinOffset)
+                precipitationTmp = 0
+                if 'rain' in timestamp:
+                    precipitationTmp += timestamp['rain']['3h']
+                if 'snow' in timestamp:
+                    precipitationTmp += timestamp['snow']['3h']
+                yAxisPrecipitation.append(precipitationTmp)
+            
+            tempAndPercipPlot = plt.figure(figsize=(720/80,200/80),dpi=80)
+            #plot percipitation
+            ax1 = tempAndPercipPlot.add_subplot()
+            xfmt1 = mdates.DateFormatter('%a')
+            locator1 = mdates.HourLocator(byhour=11)
+            ax1.plot(xAxisHours, yAxisPrecipitation, color='black')
+            ax1.fill_between(xAxisHours, 0, yAxisPrecipitation, color='black')
+            ax1.set_ylim(bottom=0)
+
+            #plot tempertatures
+            xfmt2 = mdates.DateFormatter('%H')
+            locator2 = mdates.HourLocator(byhour=range(0,24,6))
+            ax2 = ax1.twinx()
+            ax2.yaxis.tick_left()
+            ax2.plot(xAxisHours, yAxisTemperature, color='red')
+            ax2.xaxis.set_major_formatter(xfmt2)
+            ax2.xaxis.set_major_locator(locator2)
+
+            #grid lines for temperatures
+            ax2.grid(True, 'major', 'y', color="black")
+
+            #mark nights
+            nightTimestamps = []
+            i = 0
+            while True:
+                breakout = False
+                tmpSunset  = forecastJson['city']['sunset']  + i*86400
+                tmpSunrise = forecastJson['city']['sunrise'] + (i+1)*86400
+                sunset = 0
+                if tmpSunset < xAxisTimestamps[-1] and tmpSunset > xAxisTimestamps[0]:
+                    sunset = tmpSunset
+                else:
+                    if tmpSunset > xAxisTimestamps[-1]:
+                        sunset = xAxisTimestamps[-1]
+                        breakout = True
+                    if tmpSunset < xAxisTimestamps[0]:
+                        sunset = xAxisTimestamps[0]
+
+                sunrise = 0
+                if tmpSunrise < xAxisTimestamps[-1] and tmpSunrise > xAxisTimestamps[0]:
+                    sunrise = tmpSunrise
+                else:
+                    if tmpSunrise > xAxisTimestamps[-1]:
+                        sunrise = xAxisTimestamps[-1]
+                        breakout = True
+                    if tmpSunrise < xAxisTimestamps[0]:
+                        sunrise = xAxisTimestamps[0]
+
+
+                nightTimestamps.append([sunset, sunrise])
+                i += 1
+                if breakout:
+                    break 
+
+            for nightTimestamp in nightTimestamps:
+                ax2.axvspan(datetime.fromtimestamp(nightTimestamp[0]), datetime.fromtimestamp(nightTimestamp[1]), facecolor="none", edgecolor="black", hatch='....', )
+
+            for xAxisHour in xAxisHours:
+                if xAxisHour.hour == 1:
+                    ax2.axvline(xAxisHour-timedelta(hours=1), ls=':', color="red")
+
+            ax1.xaxis.set_minor_formatter(xfmt1)
+            #ax2.xaxis.set_minor_formatter(ticker.NullFormatter())
+            ax1.xaxis.set_minor_locator(locator1)
+            ax1.tick_params(axis='x', which='minor', top=False, labeltop=True, bottom=False, labelbottom=False)
+            ax1.yaxis.tick_right()
+            #fix margins
+            plt.margins(x=0)
+
+            #display
+            #plt.savefig('figure.png')
+            #canvas = plt.get_current_fig_manager().canvas
+            forecastCanvas = tempAndPercipPlot.canvas
+            forecastCanvas.draw()
+            forecastPlotImage = Image.frombytes('RGB', forecastCanvas.get_width_height(), forecastCanvas.tostring_rgb())
+            #clean up
+            plt.close(tempAndPercipPlot)
+            gc.collect(2)
+
+
+        framebufferFont30 = ImageFont.truetype('SourceCodePro-Regular.ttf', 30)
+        framebufferFont20 = ImageFont.truetype('SourceCodePro-Regular.ttf', 20)
+
+        framebufferImage = Image.new('RGB', (640, 385), (0xFF, 0xFF, 0xFF))
+        framebufferImage.paste(forecastPlotImage, (-50, 185))
+        framebufferDraw = ImageDraw.Draw(framebufferImage)
+        framebufferDraw.text((10, 0), datetime.now().strftime('%Y-%m-%d %H:%M'), font=framebufferFont30, fill=0)
+
+
+        jazzString1 = 'ČRoJazz: '+jazzJson1['data'][0]['title']+": "+jazzJson1['data'][0]['since'][11:16]+" - "+jazzJson1['data'][0]['till'][11:16]
+        framebufferDraw.text((10, 40), jazzString1, font=framebufferFont20, fill=0)
+
+        jazzString2 = '         '+jazzJson2['data']['interpret']+" - "+jazzJson2['data']['track'] if 'interpret' in jazzJson2['data'] else '         N\A'
+        framebufferDraw.text((10, 70), jazzString2, font=framebufferFont20, fill=0)
+
+
+        dustString = 'Dust:    '+str(weatherJson['current']['values'][0]['value'])+"/"+str(weatherJson['current']['values'][1]['value'])+"/"+str(weatherJson['current']['values'][2]['value'])
+        dustColor = 0 if (weatherJson['current']['standards'][0]['percent']+weatherJson['current']['standards'][1]['percent'])/2 < 100 else (255,0,0)
+        framebufferDraw.text((10, 100), dustString, font=framebufferFont20, fill=dustColor)
+        framebufferDraw.text((10, 130), 'Temp:    '+str(weatherJson['current']['values'][5]['value'])+"°", font=framebufferFont20, fill=0)
+
+        #sanitize image palette
+        framebufferImage = framebufferImage.quantize(palette=PaletteImage)
+
+        epd.Display(framebufferImage)
+        counter = counter + 1
+        epd.sleep()
+        time.sleep (60)
+
